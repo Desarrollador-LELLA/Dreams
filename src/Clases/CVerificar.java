a00c7cdd5ee4b2dc2ed444ce65dd9b2a95e0f668
Luis Llancamil <luis.llancamil.a@gmail.com>
2021-08-20 22:21:12 -0400

    AHORA SI PAPITO RICO

---
+++ src/Clases/CVerificar.java
@@ -0,0 +1,61 @@
+
+package Clases;
+
+import Objetos.OError;
+import Objetos.OVerificar;
+
+/**
+ *
+ * @author rodka
+ */
+public class CVerificar extends OVerificar{
+
+    private OError Error = new OError();
+    private String TagCodigoClase = "CVerificar - %s";
+
+    public CVerificar() {
+        super();
+    }
+
+    public CVerificar(String Rut) {
+        super(Rut);
+    }
+    
+    public OError Validar() {
+
+        int Ult = this.getRut().length();
+        int Largo = this.getRut().length() - 3;//19068257-4
+        int Constante = 2;
+        int Suma = 0;
+        int Digito = 0;
+
+        for (int i = Largo; i >= 0; i--) {
+
+            Suma = Suma + Integer.parseInt(this.getRut().substring(i, i + 1)) * Constante;
+            Constante = Constante + 1;
+            if (Constante == 8) {
+                Constante = 2;
+            }
+        }
+        String Ultimo = this.getRut().substring(Ult - 1).toUpperCase();
+        Digito = 11 - (Suma % 11);
+        if (Digito == 10 && Ultimo.equals("K")) {
+            Error = new OError(String.format(TagCodigoClase, 1), "Rut Correcto", null, true);
+            
+        } else {
+
+            if (Digito == 11 && Ultimo.equals("0")) {
+                Error = new OError(String.format(TagCodigoClase, 1), "Rut Correcto", null, true);
+            } else {
+                
+                if (Digito == Integer.parseInt(Ultimo)) {
+                   Error = new OError(String.format(TagCodigoClase, 1), "Rut Correcto", null, true);
+                }else{
+                    Error = new OError(String.format(TagCodigoClase, 1), "Rut incorrecto", null, false);
+                }
+            }
+            
+        }
+        return Error;
+    }
+}
---
+++ src/Objetos/OVerificar.java
@@ -0,0 +1,31 @@
+
+package Objetos;
+
+/**
+ *
+ * @author rodka
+ */
+public class OVerificar {
+    
+    private String Rut = "";
+    
+    
+
+    public OVerificar() {
+    }
+    
+    public OVerificar(String Rut) {
+        this.Rut=Rut;
+    }
+   
+
+    public String getRut() {
+        return Rut;
+    }
+
+    public void setRut(String Rut) {
+        this.Rut = Rut;
+    }
+    
+    
+}
