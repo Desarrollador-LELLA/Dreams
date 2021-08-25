8c9db046e7aca592bc0e8d0adcc60cc21a0bb24c
Luis Llancamil <luis.llancamil.a@gmail.com>
2021-08-25 06:29:13 -0400

    actualizacion 25-08-2021

--- src/Enum/ECliente.java
+++ src/Enum/ECliente.java
@@ -3,12 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Objetos;
+package Enum;
 
 /**
  *
- * @author Usuario
+ * @author TOULON-NOTE
  */
-public class OPack {
-    
+public enum ECliente {
+    TODOS,
+    TODOS_ACTIVOS,
+    SOLO_BUSCA_RUT    
 }
