36d5c79f9c35c1e462ef0f7d98a858ef592ba672
Luis Llancamil <luis.llancamil.a@gmail.com>
2021-09-03 04:48:04 -0400

    TESTER
    TESTEANDO TESTER

--- src/Clases/CArticulos.java
+++ src/Clases/CArticulos.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//TESTER DE TESTER DE LOS TESTER
 package Clases;
 
 import Objetos.OArticulos;
